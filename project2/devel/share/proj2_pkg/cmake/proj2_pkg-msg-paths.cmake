6f182f8b2610b859162eeece4746b1bfda35c28c
Class Account <ee106b-aan@c105-1.local>
2025-02-13 15:29:50 -0800

    project 2 started

---
+++ project2/devel/share/proj2_pkg/cmake/proj2_pkg-msg-paths.cmake
@@ -0,0 +1,4 @@
+# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
+
+set(proj2_pkg_MSG_INCLUDE_DIRS "/home/cc/ee106b/sp25/class/ee106b-aan/106b_projects/106b-projects/project2/src/proj2/msg")
+set(proj2_pkg_MSG_DEPENDENCIES geometry_msgs;std_msgs;std_srvs)
